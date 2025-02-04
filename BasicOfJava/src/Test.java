c4b6488d539703c28a1f43d2e1e04cf87e0f7b07
K Naveen <naveenkokkiligadda2001@gmail.com>
2025-02-04 12:03:03 +0530

    Static typed and Dynamic Typed Example Programming

---
+++ BasicOfJava/src/Test.java
@@ -0,0 +1,20 @@
+public class Test
+{
+
+    public static  void main(String[] args){
+
+        //Static Typed Programming Language
+        int noOfTeamMembers=10;
+        System.out.println("Total Team members ="+noOfTeamMembers);
+        noOfTeamMembers=12; // noOfTeamMembers hold same type of value only
+        System.out.println("Total Team members ="+noOfTeamMembers);
+
+//        dynamic typed
+//        number=10;
+//        number="Naveen"; in dynamic language number can hold different type of values during programing execution
+
+
+    }
+
+
+}
